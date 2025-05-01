356f4bb8ea6fd14be637a595703a4beb5d6b53dc
cnguyen50 <calvinnguyen50@gmail.com>
2025-05-01 15:10:54 -0700

    Implemented Leaflet map to bind to map div in map.html and setView to Vancouver and displays map tiles from OpenStreetMap

--- public/scripts/map.js
+++ public/scripts/map.js
@@ -1,1 +1,16 @@
 console.log("testing from inside map.js");
+
+document.addEventListener('DOMContentLoaded', () => {
+    //Init map inside div id='map'
+    const map = L.map('map').setView(
+        [49.2827, -123.1207], //set to Vancouver coords
+        10 //zoom level
+    )
+
+      // 2) Load and display tile layers on the map. Requires attribution
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: 
+            '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+        maxZoom: 19
+    }).addTo(map);
+})
